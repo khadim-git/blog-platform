7bb4c243cb274e90212cfe6f37ec83c22d129a4c
dev.edcab108-7ade-48f5-94f2-3f5362e18893 <khadim.hussain@genetechsolutions.com>
2025-12-18 17:50:31 +0500

    Fix webpack alias

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,9 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, 'src'),
-    };
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
 }
